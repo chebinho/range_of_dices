a9bcb6818b901013eb4a5d61225f21339ed03deb
chebinho <102003644+chebinho@users.noreply.github.com>
2025-11-20 13:21:53 -0300

    deu boa a nova função

--- soma_range.js
+++ soma_range.js
@@ -1,6 +1,29 @@
 // faz um array com todas as possibilidades de um range de valores
 // se existir mais de um range fornecido a função faz a soma das possibilidades
 function Calculo_em_Range_2(...range){
+
+    function menor_sequencia(valor = 0){
+        if (valor < 1) return 0 // remove valores negativos
+
+        // obtem o menor valor mais proximo dentro da sequencia 2^0,2^1,2^2,2^3,...
+        let min = Math.floor(Math.log2(valor))
+
+        let resul = []
+        resul[resul.length] = min+1
+
+        // busca o resto anterior e repete o calculo anterior até que o resto vire 0
+        let resto = valor - (2**min)
+
+        while(resto > 0){
+            min = Math.floor(Math.log2(resto))
+            resul[resul.length] = min+1
+
+            resto = resto - (2**min)
+        }
+
+        return resul.reverse()
+    }
+
     // recebe os valores em pares ou trios que são separados em duas variaveis
     const Regex = /(-?[0-9]+)[^0-9\n-]+(-?[0-9]+)([^0-9\n-]+(-?[0-9]+))?/
 
@@ -16,36 +39,76 @@ function Calculo_em_Range_2(...range){
         let Regex_$1 = Number(range[a].replace(Regex, "$1"));
         let Regex_$2 = Number(range[a].replace(Regex, "$2"));
 
+        if(Regex_$4 == 0){
 
+            let menor_val = 0
+            let maior_val = 0
+
+            // verifica qual é o maior e menor valor
+            if(Regex_$1 < Regex_$2){
+                menor_val = Regex_$1
+                maior_val = Regex_$2
+            }else{
+                menor_val = Regex_$2
+                maior_val = Regex_$1
+            }
+            
+            // cria uma range temporario simples e une ele com o resul
+            let range_temp = [[]]
+            let c = 0
+            for(let b=menor_val;b<=maior_val;b++){
+                range_temp[c] = [b,1]
+                c+=1
+            }
+
+            resul = Soma_Ranges(resul,range_temp)
+
+        }else{
+
+            let menor_val = 0
+            let maior_val = 0
+            let sequencia = menor_sequencia(Regex_$1)
+
+            // verifica qual é o maior e menor valor
+            if(Regex_$2 < Regex_$4){
+                menor_val = Regex_$2
+                maior_val = Regex_$4
+            }else{
+                menor_val = Regex_$4
+                maior_val = Regex_$2
+            }
+
+            // cria uma range temporario simples
+            let range_temp = [[]]
+            let c = 0
+            for(let b=menor_val;b<=maior_val;b++){
+                range_temp[c] = [b,1]
+                c+=1
+            }
+
+            // faz a soma dos multiplos rangens da forma mais eficiente possivel
+            if(sequencia[0] == 1){
+                resul = Soma_Ranges(resul,range_temp)
+            }
+
+            c = 1
+            for(let a=2;a<=sequencia[sequencia.length-1];a++){
+                range_temp = Soma_Ranges(range_temp,range_temp)
+                
+                if(a == sequencia[c]){
+                    resul = Soma_Ranges(resul,range_temp)
+                    c += 1
+                }
+            }
+
+            return resul
+
+        }
 
     }
 
+    return resul
 }
-
-function menor_sequencia(valor = 0){
-    if (valor < 1) return 0 // remove valores negativos
-
-    // obtem o menor valor mais proximo dentro da sequencia 2^0,2^1,2^2,2^3,...
-    let min = Math.floor(Math.log2(valor))
-
-    let resul = []
-    resul[resul.length] = min+1
-
-    // busca o resto anterior e repete o calculo anterior até que o resto vire 0
-    let resto = valor - (2**min)
-
-    while(resto > 0){
-        min = Math.floor(Math.log2(resto))
-        resul[resul.length] = min+1
-
-        resto = resto - (2**min)
-    }
-
-    return resul.reverse()
-}
-console.log(menor_sequencia(5))
-console.log(" ----------------- ")
-console.log(menor_sequencia(500))
 
 // faz um array com todas as possibilidades de um range de valores
 // se existir mais de um range fornecido a função faz a soma das possibilidades
@@ -126,12 +189,21 @@ function Calculo_em_Range(...range){
 // feito pelo chatgpt mas posteriormente adaptado
 function Soma_Ranges(range_1 = [[]], range_2=[[]]) {
 
-    //console.log(range_1)
-    //console.log(range_2)
-
-    // valida se os dados recebidos estão corretos, se não retorna um erro
+    // valida se os dados recebidos estão corretos, se não retorna um erro ou um resultado
     if( (Array.isArray(range_1) && (Array.isArray(range_2))) == false ){
         return null
+    }else if(Number.isInteger(range_1[0][0]) == false){
+        if(Number.isInteger(range_2[0][0]) == false){
+            return null
+        }else{
+            return range_2
+        }
+    }else if(Number.isInteger(range_2[0][0]) == false){
+        if(Number.isInteger(range_1[0][0]) == false){
+            return null
+        }else{
+            return range_1
+        }
     }
 
     const n = range_1.length;  // número de linhas
